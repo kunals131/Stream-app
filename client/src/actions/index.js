543a8e9b7f16b487ddc84f53ae145f974fe63532
kunals131 <86314140+kunals131@users.noreply.github.com>
2021-12-14 11:14:47 +0530

    Wired up all Action Creators according to REST Convections

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -1,6 +1,6 @@
-import { SIGN_IN, SIGN_OUT, CREATE_STREAM } from "./types"
+import { SIGN_IN, SIGN_OUT, CREATE_STREAM, EDIT_STREAM, DELETE_STREAM, FETCH_STREAM, FETCH_STREAMS } from "./types"
 import streams from '../apis/streams';
-import { formValues } from "redux-form";
+
 
 export const signIn = (id)=>{
     return{
@@ -16,15 +16,28 @@ export const signOut = ()=>{
 }
 
 export const createStream = formValues=> async dispatch=>{
-    let axiosConfig = {
-        headers: {
-            'Content-Type': 'application/json',
-        },
-        mode : 'cors',
-        cache : 'no-cache',
-        
-      };
-    const res = await streams.post('/streams', JSON.stringify(formValues), axiosConfig);
-    dispatch({type : CREATE_STREAM, payload : res.data})
+    const res = await streams.post('/streams', JSON.stringify(formValues));
+    dispatch({type : CREATE_STREAM, payload : res.data});
+}
 
+export const fetchStreams = ()=>async dispatch=>{
+    const res = await streams.get('/streams');
+
+    dispatch({type : FETCH_STREAMS, payload : res.data});
+}
+
+export const fetchStream = (id)=>async dispatch =>{
+    const res = await streams.get(`/streams/${id}`);
+    dispatch({type : FETCH_STREAM, payload : res.data});
+}
+
+export const editStream = (id,formValues)=>async dispatch=>{
+    const res = await streams.put(`/streams/${id}`, formValues);
+    dispatch({type : EDIT_STREAM, payload : res.data});
+}
+
+export const deleteStream = (id)=>async dispatch=>{
+    await streams.delete(`/streams/${id}`);
+
+    dispatch({type : DELETE_STREAM, payload : id});
 }
