84b636d2aeeef81c23f04bfe43c2cf69757100c2
kunals131 <86314140+kunals131@users.noreply.github.com>
2021-12-14 11:50:35 +0530

    Added Stream Reducers and Lodash Library

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from "redux";
 import AuthReducer from "./AuthReducer";
 import {reducer as formReducer} from 'redux-form';
+import streamReducer from "./streamReducer";
 
 export default combineReducers({
     authStatus : AuthReducer,
     form : formReducer,
+    streams : streamReducer,
 })
