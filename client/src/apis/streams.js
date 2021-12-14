534d9d2b90a11059943a3f2358125ae6b11e9ef0
kunals131 <86314140+kunals131@users.noreply.github.com>
2021-12-14 16:50:25 +0530

    Fixed Json Type

--- client/src/apis/streams.js
+++ client/src/apis/streams.js
@@ -2,5 +2,9 @@ import axios from 'axios';
 
 export default axios.create({
     baseURL : 'http://localhost:3001',
-
+    headers: {
+        'Content-Type': 'application/json',
+    },
+    mode : 'cors',
+    cache : 'no-cache',
 })
