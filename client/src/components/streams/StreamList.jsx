a5134898f01243fe51d76fdb08a1e42a126a4c1c
kunals131 <86314140+kunals131@users.noreply.github.com>
2021-12-14 12:00:20 +0530

    Rendered Stream List

--- client/src/components/streams/StreamList.jsx
+++ client/src/components/streams/StreamList.jsx
@@ -1,11 +1,38 @@
 import React from 'react'
+import { fetchStreams } from '../../actions'
+import { connect } from 'react-redux'
+import { useEffect } from 'react'
+const StreamList = (props) => {
 
-const StreamList = () => {
+    useEffect(()=>{
+        props.fetchStreams();
+    },[])
+    const renderList = ()=>{
+        return props.streams.map(stream=>{
+            return (
+                <div className="item" key={stream.id}>
+                    <i className="large middle aligned icon camera"></i>
+                    <div className="content">
+                        {stream.title}
+                        <div className="description">{stream.description}</div>
+                    </div>
+                </div>
+            )
+        })
+    }
     return (
         <div>
-            StreamList
+            <h2>Streams</h2>
+            <div className="ui celled list">
+                {renderList()}
+            </div>
         </div>
     )
 }
 
-export default StreamList
+
+const mapStateToProps = (state)=>{
+    return {streams : Object.values(state.streams)};
+    //Object.values turns all object values into an array
+}
+export default connect(mapStateToProps, {fetchStreams})(StreamList);
