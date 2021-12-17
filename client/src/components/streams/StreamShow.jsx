3fb2b3b4b377089f258fc1ff1beb9a199e9b11a3
kunals131 <86314140+kunals131@users.noreply.github.com>
2021-12-17 20:08:05 +0530

    Implemeted Streaming Functionality

--- client/src/components/streams/StreamShow.jsx
+++ client/src/components/streams/StreamShow.jsx
@@ -1,19 +1,49 @@
-import React, { useEffect } from 'react'
+import React, { useEffect, useRef } from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
+import * as flv from 'flv.js'
 import { fetchStream } from '../../actions'
+import { useState } from 'react'
 
 const StreamShow = (props) => {
+    const videoRef = useRef();
     const id = props.match.params.id;
+   
+    const flvPlayer = flv.createPlayer({
+        type : 'flv',
+        url : `http://localhost:8000/live/${id}.flv`,
+    })
+    const buildplayer = ()=> {
+        if (!props.stream) {
+            return;
+        }
+        flvPlayer.attachMediaElement(videoRef.current);
+        flvPlayer.load(); 
+    }
+
     useEffect(()=>{
-        props.fetchStream(id)  
+        props.fetchStream(id) 
+        buildplayer();
+
+        return ()=>{
+            console.log('I was unmounted')
+            flvPlayer.destroy();
+        }
+
     },[])
+
+    useEffect(()=>{
+        buildplayer();
+    }, [props.stream])
+
     if (!props.stream) {
         return <div>Loading...</div>
     }
+
     const {title,description} = props.stream
     return (
         <div>
+            <video ref={videoRef} style={{width : '100%'}} controls/>
             <h1>{title}</h1>
             <h5>{description}</h5>
         </div>
