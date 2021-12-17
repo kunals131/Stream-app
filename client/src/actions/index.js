16d81cd17c41e7149b4450ef1b436166e3f8bd55
kunals131 <86314140+kunals131@users.noreply.github.com>
2021-12-17 19:03:01 +0530

    Added Delete Stream Functionality

--- client/src/actions/index.js
+++ client/src/actions/index.js
@@ -46,5 +46,6 @@ export const deleteStream = (id)=>async dispatch=>{
     await streams.delete(`/streams/${id}`);
 
     dispatch({type : DELETE_STREAM, payload : id});
+    history.push('/');
 }
 
--- client/src/components/streams/StreamDelete.jsx
+++ client/src/components/streams/StreamDelete.jsx
@@ -1,11 +1,55 @@
-import React from 'react'
+import React from "react";
+import Modal from "./Modal";
+import history from "../../history";
+import { withRouter, Link } from "react-router-dom";
+import { useEffect } from "react";
+import { fetchStream, deleteStream } from "../../actions";
 
-const StreamDelete = () => {
-    return (
-        <div>
-            StreamDelete
-        </div>
-    )
-}
+import { connect } from "react-redux";
+const StreamDelete = (props) => {
+  const renderContent = () => {
+    if (!props.streams) {
+      return "Are you sure you want to delete this stream?";
+    } else {
+      return `Are you sure you want to delete the stream with title : ${props.streams.title}`;
+    }
+  };
+  const id = props.match.params.id;
 
-export default StreamDelete
+  useEffect(() => {
+    props.fetchStream(id);
+  }, []);
+  const actions = (
+    <>
+      <button
+        onClick={() => props.deleteStream(id)}
+        className="ui button negative"
+      >
+        Delete
+      </button>
+      <Link to="/" className="ui button">
+        Cancel
+      </Link>
+    </>
+  );
+
+  return (
+    <>
+      <Modal
+        onDismiss={() => history.push("/")}
+        title="Delete Stream"
+        content={renderContent()}
+        actions={actions}
+      />
+    </>
+  );
+};
+const mapStateToProps = (state, ownProps) => {
+  return {
+    streams: state.streams[ownProps.match.params.id],
+  };
+};
+
+export default withRouter(
+  connect(mapStateToProps, { fetchStream, deleteStream })(StreamDelete)
+);
